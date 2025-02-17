42ff992a941fabf684e330f901201d91888ce45a
Gopal20004 <gopuverma345@gmail.com>
2025-02-17 16:57:21 +0530

    created

---
+++ the-java-academy-bank/src/main/java/com/musdon/the_java_academy_bank/TheJavaAcademyBankApplication.java
@@ -0,0 +1,13 @@
+package com.musdon.the_java_academy_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaAcademyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheJavaAcademyBankApplication.class, args);
+	}
+
+}
